24f6e8edc665411e155b88d0ebb337ef0f33a9b4
val461 <6820182+val461@users.noreply.github.com>
2025-12-19 08:49:02 +0000

    dvc stage add

--- DVC.sh
+++ DVC.sh
@@ -3,8 +3,9 @@ dvc stage add -n split \
               -o data/processed_data/X_train.csv -o data/processed_data/X_test.csv -o data/processed_data/y_train.csv -o data/processed_data/y_test.csv \
               python src/data/split.py
 
-# TODO: specify output files, not folder
 dvc stage add -n normalize \
               -d src/data/normalize.py -d data/processed_data/X_train.csv -d data/processed_data/X_test.csv \
-              -o data/processed_data \
+              -o data/processed_data/X_train_scaled.csv -o data/processed_data/X_test_scaled.csv \
               python src/data/normalize.py
+
+# TODO
