bd5a7b5ffc322b271b3f14636c57db3c39761002
val461 <6820182+val461@users.noreply.github.com>
2025-12-19 10:22:15 +0000

    dvc add

--- DVC.sh
+++ DVC.sh
@@ -1,5 +1,5 @@
 dvc stage add -n split \
-              -d src/data/split.py -d data/raw_data \
+              -d src/data/split.py -d data/raw_data/raw.csv \
               -o data/processed_data/X_train.csv -o data/processed_data/X_test.csv -o data/processed_data/y_train.csv -o data/processed_data/y_test.csv \
               python src/data/split.py
 
@@ -7,5 +7,20 @@ dvc stage add -n normalize \
               -d src/data/normalize.py -d data/processed_data/X_train.csv -d data/processed_data/X_test.csv \
               -o data/processed_data/X_train_scaled.csv -o data/processed_data/X_test_scaled.csv \
               python src/data/normalize.py
+
+dvc stage add -n grid_search \
+              -d src/models/grid_search.py -d data/processed_data/X_train_scaled.csv -d data/processed_data/y_train.csv \
+              -o models/best_params.pkl \
+              python src/models/grid_search.py
+
+dvc stage add -n train \
+              -d src/models/train.py -d models/best_params.pkl -d data/processed_data/X_train_scaled.csv -d data/processed_data/y_train.csv \
+              -o models/trained_model.pkl \
+              python src/models/train.py
+
+dvc stage add -n eval \
+              -d models/trained_model.pkl -d data/processed_data/X_test_scaled.csv -d data/processed_data/y_test.csv \
+              -o data/y_pred.csv -M metrics/scores.json \
+              python src/models/eval.py
 
 # TODO
